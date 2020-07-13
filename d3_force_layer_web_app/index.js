e0f4c09405299993b7af355f98674f81046daa7e
Teng Fu <teng.fu@teleware.com>
2020-07-13 15:35:07 +0100

    lesson 3 adding links between nodes

--- d3_force_layer_web_app/index.js
+++ d3_force_layer_web_app/index.js
@@ -1,4 +1,15 @@
-d3.json("data.json").then(function (data) {
+// d3.json("data.json").then( (data) => {
+d3.json("org.json").then(function (data) {
+    // define scale
+    var linkWidthScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(data.links.map(function (link) { return link.weight; }))])
+        .range([0.5, 3]);
+    // define dash links
+    var linkDashScale = d3
+        .scaleOrdinal()
+        .domain([0, 2, 3])
+        .range(["4 2", "2 2", null]);
     // console.log("Got data?", data);
     var simulation = d3.forceSimulation(data.nodes)
         .force("charge", d3.forceManyBody().strength(-100))
@@ -7,6 +18,25 @@ d3.json("data.json").then(function (data) {
         .distance(100))
         .force("center", d3.forceCenter(300, 300));
     var svg = d3.select("#Target");
+    // draw links first
+    var link = svg
+        .selectAll("path.link")
+        .data(data.links)
+        .enter()
+        .append("path")
+        .attr("stroke", "black")
+        .attr("stroke-width", function (d) { return linkWidthScale(d.weight); })
+        .attr("strok-dasharray", function (d) { return linkDashScale(d.weight); })
+        .attr("fill", "none")
+        .attr("marker-mid", function (d) {
+        switch (d.type) {
+            case "SUPERVISORY":
+                return "url(#markerArrow)";
+            default:
+                return "none";
+        }
+    });
+    // draw nodes later that covers links connection
     var node = svg
         .selectAll("circle")
         .data(data.nodes)
@@ -16,24 +46,37 @@ d3.json("data.json").then(function (data) {
         .attr("stroke", "green")
         .attr("stroke-width", 0.5)
         .style("fill", "blue");
-    var link = svg
-        .selectAll("path.link")
-        .data(data.links)
-        .enter()
-        .append("path")
-        .attr("stroke", "black")
-        .attr("fill", "none");
     var lineGenerator = d3.line();
     simulation.on("tick", function () {
         console.log("Tick");
         node
             .attr("cx", function (d) { return d.x; })
-            .attr("cy", (function (d) { return d.y; }));
+            .attr("cy", function (d) { return d.y; });
         link.attr("d", function (d) {
-            return lineGenerator([
-                [d.source.x, d.source.y],
-                [d.target.x, d.target.y]
-            ]);
+            var mid = [
+                (d.source.x + d.target.x) / 2,
+                (d.source.y + d.target.y) / 2
+            ];
+            if ()
+                debugger;
+            overlap > 0;
         });
+        {
+            var distance = Math.sqrt(Math.pow(d.target.x - d.source.x, 2) +
+                Math.pow(d.target.y - d.source.y));
+        }
     });
+    var slopeX = (d.target.x - d.source.x) / distance;
+    var slopeY = (d.target.y - d.source.y) / distance;
+    var curveSharpness = 3;
+    mid[0] += sloptY * curveSharpness;
+    mid[1] += slopeX * curveSharpness;
 });
+return lineGenerator([
+    [d.source.x, d.source.y],
+    mid,
+    [d.target.x, d.target.y]
+]);
+;
+;
+;
--- d3_force_layer_web_app/index.ts
+++ d3_force_layer_web_app/index.ts
@@ -1,4 +1,18 @@
-d3.json("data.json").then( (data) => {
+// d3.json("data.json").then( (data) => {
+d3.json("org.json").then( (data) => {
+
+    // define scale
+    const linkWidthScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(data.links.map(link => link.weight))])
+        .range([0.5, 3]);
+
+    // define dash links
+    const linkDashScale = d3
+        .scaleOrdinal()
+        .domain([0, 2, 3])
+        .range(["4 2", "2 2", null]);
+
     // console.log("Got data?", data);
     const simulation = d3.forceSimulation(data.nodes)
         .force("charge", d3.forceManyBody().strength(-100))
@@ -9,6 +23,26 @@ d3.json("data.json").then( (data) => {
     
     const svg = d3.select("#Target");
 
+    // draw links first
+    const link = svg
+    .selectAll("path.link")
+    .data(data.links)
+    .enter()
+    .append("path")
+    .attr("stroke", "black")
+    .attr("stroke-width", (d)=> linkWidthScale(d.weight))
+    .attr("strok-dasharray", (d)=> linkDashScale(d.weight))
+    .attr("fill", "none")
+    .attr("marker-mid", (d)=>{
+        switch(d.type) {
+            case "SUPERVISORY":
+                return "url(#markerArrow)";
+            default:
+                return "none";
+        }
+    });
+
+    // draw nodes later that covers links connection
     const node = svg
         .selectAll("circle")
         .data(data.nodes)
@@ -19,25 +53,39 @@ d3.json("data.json").then( (data) => {
         .attr("stroke-width", 0.5)
         .style("fill", "blue");
 
-    const link = svg
-        .selectAll("path.link")
-        .data(data.links)
-        .enter()
-        .append("path")
-        .attr("stroke", "black")
-        .attr("fill", "none");
-    
+
     const lineGenerator = d3.line();
 
     simulation.on("tick", () => {
         console.log("Tick");
         node
             .attr("cx", (d) => d.x)
-            .attr("cy", (d => d.y));
+            .attr("cy", (d) => d.y);
         
         link.attr("d", (d) => {
+
+            const mid = [
+               (d.source.x + d.target.x)/2,
+               (d.source.y + d.target.y)/2
+            ];
+
+            if (debugger.overlap>0) {
+                const distance = Math.sqrt(
+                    Math.pow(d.target.x - d.source.x, 2) + 
+                    Math.pow(d.target.y - d.source.y);
+                );
+            
+                const slopeX = (d.target.x - d.source.x) / distance;
+                const slopeY = (d.target.y - d.source.y) / distance;
+
+                const curveSharpness = 3;
+                mid[0] += sloptY * curveSharpness;
+                mid[1] += slopeX * curveSharpness;
+            };
+
             return lineGenerator([
                 [d.source.x, d.source.y],
+                mid,
                 [d.target.x, d.target.y]
             ]);
         });
