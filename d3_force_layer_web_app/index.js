7735ee03eae89c125e38bc681ca93ea357f00bc9
Teng Fu <teng.fu@teleware.com>
2020-07-13 22:38:23 +0100

    lesson_5 styling nodes, interaction of hover, drag and drop

--- d3_force_layer_web_app/index.js
+++ d3_force_layer_web_app/index.js
@@ -1,22 +1,54 @@
 // d3.json("data.json").then( (data) => {
 d3.json("org.json").then(function (data) {
+    var drag = function (simulation) {
+        var dragstarted = function (d) {
+            if (!d3.event.active) {
+                simulation.alphaTarget(0.3).restart();
+            }
+            d.fx = d.x;
+            d.fy = d.y;
+        };
+        var dragged = function (d) {
+            d.fx = d3.event.x;
+            d.fy = d3.event.y;
+        };
+        var dragended = function (d) {
+            if (!d3.event.active) {
+                simulation.alphaTarget(0);
+            }
+            d.fx = null;
+            d.fy = null;
+        };
+        return d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended);
+    };
     // define scale
     var linkWidthScale = d3
         .scaleLinear()
         .domain([0, d3.max(data.links.map(function (link) { return link.weight; }))])
-        .range([0.5, 3]);
+        .range([0.5, 1.25]);
     // define dash links
     var linkDashScale = d3
         .scaleOrdinal()
         .domain([0, 2, 3])
         .range(["4 2", "2 2", null]);
+    var nodeScale = d3.scaleLinear()
+        .domain([0, d3.max(data.nodes.map(function (node) { return node.influence; }))])
+        .range([8, 20]);
+    var fontSizeScale = d3.scaleLinear()
+        .domain([0, d3.max(data.nodes.map(function (node) { return node.influence; }))])
+        .range([7, 12]);
+    var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
     // console.log("Got data?", data);
     var simulation = d3.forceSimulation(data.nodes)
-        .force("charge", d3.forceManyBody().strength(-100))
+        .force("charge", d3.forceManyBody().strength(-75))
         .force("link", d3.forceLink(data.links)
         .id(function (d) { return d.id; })
         .distance(100))
-        .force("center", d3.forceCenter(300, 300));
+        .force("center", d3.forceCenter(300, 300))
+        .force("gravity", d3.forceManyBody().strength(7.5));
     var svg = d3.select("#Target");
     // draw links first
     var link = svg
@@ -24,9 +56,9 @@ d3.json("org.json").then(function (data) {
         .data(data.links)
         .enter()
         .append("path")
-        .attr("stroke", "black")
+        .attr("stroke", "#999")
+        .attr("stroke-dasharray", function (d) { return linkDashScale(d.weight); })
         .attr("stroke-width", function (d) { return linkWidthScale(d.weight); })
-        .attr("strok-dasharray", function (d) { return linkDashScale(d.weight); })
         .attr("fill", "none")
         .attr("marker-mid", function (d) {
         switch (d.type) {
@@ -42,13 +74,78 @@ d3.json("org.json").then(function (data) {
         .data(data.nodes)
         .enter()
         .append("circle")
-        .attr("r", 15)
-        .attr("stroke", "green")
+        .attr("r", function (d) { return nodeScale(d.influence); })
+        .attr("stroke", "#ccc")
         .attr("stroke-width", 0.5)
-        .style("fill", "blue");
-    var lineGenerator = d3.line();
+        .style("fill", function (d) { return colorScale(d.zone); });
+    node.call(drag(simulation));
+    // const imageContainer = svg
+    //     .selectAll("g.imageContainer")
+    //     .data(data.nodes)
+    //     .enter()
+    //     .append("g");
+    // const image = imageContainer
+    //     .append("image")
+    //     .attr("height", (d) => nodeScale(d.influence))
+    //     .attr("width", (d) => nodeScale(d.influence))
+    //     .attr("transform", (d) =>`translate(${-nodeScale(d.influence)/2}, ${-nodeScale(d.influence)/2})`)
+    //     .attr("href", (d, i) => `image/img-${i}.png`);
+    var textContainer = svg
+        .selectAll("g.label")
+        .data(data.nodes)
+        .enter()
+        .append("g");
+    textContainer
+        .append("text")
+        .text(function (d) { return d.name; })
+        .attr("font-size", function (d) { return fontSizeScale(d.influence); })
+        .attr("transform", function (d) {
+        var scale = nodeScale(d.influence);
+        var x = scale + 2;
+        var y = scale + 4;
+        return "translate($(x), $(y))";
+    });
+    var card = svg
+        .append("g")
+        .attr("pointer-events", "none")
+        .attr("display", "none");
+    var cardBackground = card.append("rect")
+        .attr("width", 180)
+        .attr("height", 45)
+        .attr("fill", "#eee")
+        .attr("stroke", "#333")
+        .attr("rx", 4);
+    var cardTextName = card
+        .append("text")
+        .attr("transform", "translate(8, 20)");
+    var cardTextRole = card
+        .append("text")
+        .attr("font-size", "10")
+        .attr("transform", "translate(8, 35)");
+    var currentTarget = null;
+    node.on("mouseover", function (d) {
+        currentTarget = d3.event.target;
+        card.attr("display", "block");
+        cardTextName.text(d.name);
+        cardTextRole.text(d.role);
+        var nameWidth = cardTextName.node().getBBox().width;
+        var positionWidth = cardTextRole.node().getBBox().width;
+        var cardWidth = Math.max(nameWidth, positionWidth);
+        cardBackground.attr("width", cardWidth + 16);
+        simulation.alphTarget(0).restart();
+    });
+    node.on("mouseout", function () {
+        card.attr("display", "none");
+        currentTarget = null;
+    });
+    var lineGenerator = d3.line()
+        .curve(d3.curveCardinal);
     simulation.on("tick", function () {
         console.log("Tick");
+        textContainer
+            .attr("transform", function (d) { return "translate(" + d.x + ", " + d.y + ")"; });
+        // imageContainer
+        //     .attr("transform", (d) => `translate(${d.x}, ${d.y})`);
         node
             .attr("cx", function (d) { return d.x; })
             .attr("cy", function (d) { return d.y; });
@@ -57,26 +154,27 @@ d3.json("org.json").then(function (data) {
                 (d.source.x + d.target.x) / 2,
                 (d.source.y + d.target.y) / 2
             ];
-            if ()
-                debugger;
-            overlap > 0;
+            if (d.overlap > 0) {
+                var distance = Math.sqrt(Math.pow(d.target.x - d.source.x, 2) +
+                    Math.pow(d.target.y - d.source.y, 2));
+                var slopeX = (d.target.x - d.source.x) / distance;
+                var slopeY = (d.target.y - d.source.y) / distance;
+                var curveSharpness = 10;
+                mid[0] += slopeY * curveSharpness;
+                mid[1] += slopeX * curveSharpness;
+            }
+            ;
+            return lineGenerator([
+                [d.source.x, d.source.y],
+                mid,
+                [d.target.x, d.target.y]
+            ]);
         });
-        {
-            var distance = Math.sqrt(Math.pow(d.target.x - d.source.x, 2) +
-                Math.pow(d.target.y - d.source.y));
+        if (currentTarget) {
+            var radius = currentTarget.r.baseVal.value;
+            var xPos = currentTarget.cx.baseVal.value + radius + 3;
+            var yPos = currentTarget.cy.baseVal.value + radius + 3;
+            card.attr("transform", "translate(" + xPos + ", " + yPos + ")");
         }
     });
-    var slopeX = (d.target.x - d.source.x) / distance;
-    var slopeY = (d.target.y - d.source.y) / distance;
-    var curveSharpness = 3;
-    mid[0] += sloptY * curveSharpness;
-    mid[1] += slopeX * curveSharpness;
 });
-return lineGenerator([
-    [d.source.x, d.source.y],
-    mid,
-    [d.target.x, d.target.y]
-]);
-;
-;
-;
--- d3_force_layer_web_app/index.ts
+++ d3_force_layer_web_app/index.ts
@@ -1,11 +1,39 @@
 // d3.json("data.json").then( (data) => {
 d3.json("org.json").then( (data) => {
 
+    const drag = simulation => {
+        const dragstarted = d => {
+            if(!d3.event.active) {
+                simulation.alphaTarget(0.3).restart();
+            }
+            d.fx = d.x;
+            d.fy = d.y;
+        }
+
+        const dragged = d => {
+            d.fx = d3.event.x;
+            d.fy = d3.event.y;
+        };
+
+        const dragended = (d) => {
+            if (!d3.event.active) {
+                simulation.alphaTarget(0);
+            }
+
+            d.fx = null;
+            d.fy = null;
+        }
+
+        return d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended)
+    }
     // define scale
     const linkWidthScale = d3
         .scaleLinear()
         .domain([0, d3.max(data.links.map(link => link.weight))])
-        .range([0.5, 3]);
+        .range([0.5, 1.25]);
 
     // define dash links
     const linkDashScale = d3
@@ -13,13 +41,25 @@ d3.json("org.json").then( (data) => {
         .domain([0, 2, 3])
         .range(["4 2", "2 2", null]);
 
+    const nodeScale = d3.scaleLinear()
+        .domain([0, d3.max(data.nodes.map(node => node.influence))])
+        .range([8, 20]);
+    
+    const fontSizeScale = d3.scaleLinear()
+        .domain([0, d3.max(data.nodes.map(node => node.influence))])
+        .range([7, 12]);
+
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
     // console.log("Got data?", data);
     const simulation = d3.forceSimulation(data.nodes)
-        .force("charge", d3.forceManyBody().strength(-100))
+        .force("charge", d3.forceManyBody().strength(-75))
         .force("link", d3.forceLink(data.links)
             .id(d=> d.id)
             .distance(100))
-        .force("center", d3.forceCenter(300, 300));
+        .force("center", d3.forceCenter(300, 300))
+        .force("gravity", d3.forceManyBody().strength(7.5));
+         
     
     const svg = d3.select("#Target");
 
@@ -29,9 +69,9 @@ d3.json("org.json").then( (data) => {
     .data(data.links)
     .enter()
     .append("path")
-    .attr("stroke", "black")
+    .attr("stroke", "#999")
+    .attr("stroke-dasharray", (d) => linkDashScale(d.weight))
     .attr("stroke-width", (d)=> linkWidthScale(d.weight))
-    .attr("strok-dasharray", (d)=> linkDashScale(d.weight))
     .attr("fill", "none")
     .attr("marker-mid", (d)=>{
         switch(d.type) {
@@ -48,16 +88,103 @@ d3.json("org.json").then( (data) => {
         .data(data.nodes)
         .enter()
         .append("circle")
-        .attr("r", 15)
-        .attr("stroke", "green")
+        .attr("r", (d) => nodeScale(d.influence))
+        .attr("stroke", "#ccc")
         .attr("stroke-width", 0.5)
-        .style("fill", "blue");
+        .style("fill", (d) => colorScale(d.zone));
+    
+    node.call(drag(simulation));
+
+    // const imageContainer = svg
+    //     .selectAll("g.imageContainer")
+    //     .data(data.nodes)
+    //     .enter()
+    //     .append("g");
+
+    // const image = imageContainer
+    //     .append("image")
+    //     .attr("height", (d) => nodeScale(d.influence))
+    //     .attr("width", (d) => nodeScale(d.influence))
+    //     .attr("transform", (d) =>`translate(${-nodeScale(d.influence)/2}, ${-nodeScale(d.influence)/2})`)
+    //     .attr("href", (d, i) => `image/img-${i}.png`);
 
 
-    const lineGenerator = d3.line();
+    const textContainer = svg
+        .selectAll("g.label")
+        .data(data.nodes)
+        .enter()
+        .append("g");
+    
+    textContainer
+        .append("text")
+        .text((d) => d.name)
+        .attr("font-size", (d) => fontSizeScale(d.influence))
+        .attr("transform", (d) => {
+            const scale = nodeScale(d.influence);
+            const x = scale + 2;
+            const y = scale + 4;
+            return `translate($(x), $(y))`
+        })
+    
+    const card = svg
+        .append("g")
+        .attr("pointer-events", "none")
+        .attr("display", "none");
+    
+    const cardBackground = card.append("rect")
+        .attr("width", 180)
+        .attr("height", 45)
+        .attr("fill", "#eee")
+        .attr("stroke", "#333")
+        .attr("rx", 4);
+    
+    const cardTextName = card
+        .append("text")
+        .attr("transform", "translate(8, 20)");
+
+    
+    const cardTextRole = card
+        .append("text")
+        .attr("font-size", "10")
+        .attr("transform", "translate(8, 35)");
+    
+    let currentTarget = null;
+
+    node.on("mouseover", d=> {
+        currentTarget = d3.event.target;
+        card.attr("display", "block");
+
+        cardTextName.text(d.name);
+        cardTextRole.text(d.role);
+
+        const nameWidth = cardTextName.node().getBBox().width;
+        const positionWidth = cardTextRole.node().getBBox().width;
+
+        const cardWidth = Math.max(nameWidth, positionWidth);
+
+        cardBackground.attr("width", cardWidth + 16);
+
+        simulation.alphTarget(0).restart();
+
+    })
+
+    node.on("mouseout", () => {
+        card.attr("display", "none");
+        currentTarget = null;
+    })
+
+    const lineGenerator = d3.line()
+        .curve(d3.curveCardinal);
 
     simulation.on("tick", () => {
         console.log("Tick");
+
+        textContainer
+            .attr("transform", (d) => `translate(${d.x}, ${d.y})`);
+
+        // imageContainer
+        //     .attr("transform", (d) => `translate(${d.x}, ${d.y})`);
+
         node
             .attr("cx", (d) => d.x)
             .attr("cy", (d) => d.y);
@@ -69,17 +196,17 @@ d3.json("org.json").then( (data) => {
                (d.source.y + d.target.y)/2
             ];
 
-            if (debugger.overlap>0) {
+            if (d.overlap>0) {
                 const distance = Math.sqrt(
                     Math.pow(d.target.x - d.source.x, 2) + 
-                    Math.pow(d.target.y - d.source.y);
+                    Math.pow(d.target.y - d.source.y, 2)
                 );
             
                 const slopeX = (d.target.x - d.source.x) / distance;
                 const slopeY = (d.target.y - d.source.y) / distance;
 
-                const curveSharpness = 3;
-                mid[0] += sloptY * curveSharpness;
+                const curveSharpness = 10;
+                mid[0] += slopeY * curveSharpness;
                 mid[1] += slopeX * curveSharpness;
             };
 
@@ -87,7 +214,16 @@ d3.json("org.json").then( (data) => {
                 [d.source.x, d.source.y],
                 mid,
                 [d.target.x, d.target.y]
-            ]);
+            ])
         });
+
+        if (currentTarget){
+            const radius = currentTarget.r.baseVal.value;
+            const xPos = currentTarget.cx.baseVal.value + radius +3;
+            const yPos = currentTarget.cy.baseVal.value + radius +3;
+
+            card.attr("transform", `translate(${xPos}, ${yPos})`);
+
+        }
     });
 });
